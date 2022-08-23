0583e9e5a111e51be535d67ffa2bbb8cb45d12f8
brandon <brandon.t.hunt@utah.edu>
2022-08-23 11:18:04 -0600

    auto record length and filesize incrementing are implemented and functional

--- ezRx.py
+++ ezRx.py
@@ -3,6 +3,7 @@ import numpy as np
 import datetime as dt
 import queue
 import tkinter as tk
+import uuid
 from threading import *
 
 class ezRxWindow(tk.Tk):
@@ -10,20 +11,18 @@ class ezRxWindow(tk.Tk):
     # empty list for streamed samples
     rx_gain = 8
     offset_freq = 100000
-    rx_rate = 1000000
+    rx_rate = 100e6/100
+    fname = []
+    dir = 'rxBins/'
 
-    def __init__(self, fname, fc):
+    def __init__(self):
         # create tkinter window
         super().__init__()
-        self.geometry("200x400")
+        self.geometry("200x225")
 
         # initialize Queue and begin polling
         self.queue = queue.Queue()
         self.after(100, self.checkQueue)
-
-        # copy/include parameters
-        self.fc = fc + self.offset_freq
-        self.fname = fname
 
         """ GUI/HMI Interactivity Construction """
         # create fc and textbox
@@ -33,13 +32,13 @@ class ezRxWindow(tk.Tk):
         self.fcbox.pack(pady=5)
 
         # create fname label and dropdown
-        self.droplab = tk.Label(self, text="Packet number: ")
+        self.droplab = tk.Label(self, text="Packet selection: ")
         self.droplab.pack(pady=1)
-        clicked = tk.StringVar()
-        clicked.set("Click to select")                                # default string
-        nPackets = 10                                                 # number of packet options to support
-        options = ["Packet " + str(i) for i in range(nPackets)]       # packet selection options
-        self.drop = tk.OptionMenu(self, clicked, *options)
+        self.clicked = tk.StringVar()
+        self.clicked.set("Click to select")                                # default string
+        self.nPackets = 10                                                 # number of packet options to support
+        self.pktOptions = ["Packet " + str(i) for i in range(self.nPackets)]       # packet selection options
+        self.drop = tk.OptionMenu(self, self.clicked, *self.pktOptions)
         self.drop.pack()
 
         # create button & assoc. command
@@ -66,13 +65,24 @@ class ezRxWindow(tk.Tk):
         try:
             fchz = float(self.fcbox.get("1.0", "end-1c"))
             self.fc = 1000*fchz
-            return 0
         except ValueError:
-            self.errLab['text'] = "Invalid center freq.!"
+            self.errLab['text'] = "Invalid center frequency!"
             self.errLab['fg'] = "#e00"
             return 1
 
-        # read fname dropdown
+        # some values are hardcoded. thay are unexpected to change in the future
+        if self.clicked.get() == "Click to select":
+            self.errLab['text'] = "Must select packet!"
+            self.errLab['fg'] = "#e00"
+            return 1
+        else:
+            fnamesel = self.clicked.get()
+            fNameOpts = ["pkt" + str(i) for i in range(self.nPackets)]
+            pktSel = fNameOpts[self.pktOptions.index(fnamesel)]            # shorten name to "pktX" where X \elem [0, nPackets-1]
+            utc = dt.datetime.utcnow()
+            self.utcStr = utc.strftime("_%b%d_%H%M")
+            ftype = '.bin'
+            self.fname = self.dir + pktSel + self.utcStr + ftype
 
 
     def checkQueue(self):
@@ -89,29 +99,51 @@ class ezRxWindow(tk.Tk):
                 print("Acknowledging button press... beginning recording")
                 errFlag = self.assertParameters()
 
-                # if an error was thrown, return to polling queue
+                # if an error was thrown when assigning parameters, return to polling queue
                 if errFlag:
                     self.queue.task_done()
                     self.checkQueue()
                     return
 
-                print("TODO: COPY SELF PARAMS BEFORE LAUNCH")
+                # Create logger file
+                self.writeLog()
+
+                # Destroy current window, spawn stream-record window from rxContinuous.py
+                self.queue.task_done()
                 self.destroy()
                 sfr(self.rx_rate,
                     self.fname,
                     self.fc,
                     self.rx_gain,
-                    self.offset_freq)
-
-                self.queue.task_done()
-
+                    self.offset_freq,
+                    2**4,
+                    0.05)
             else:
                 # "catch" unknown queue elements.
                 print('unknown item in queue...')
 
+    def writeLog(self):
+        # Open/create text file for logging
+        with open(self.dir + 'rxLog.txt', 'a') as f:
+            # fairly jank MAC address reformatting
+            macFormat = [':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff) for ele in range(0, 8 * 6, 8)][::-1])]
+            macString = ''.join(macFormat)
+            f.write('\n')
+            f.write('==================================================\n')
+            row = "New recording started at " + self.utcStr[1:] + " from machine with MAC of " + macString + ".\n"
+            f.write(row)
+            row = "packet number: " + self.clicked.get() + ". \nfc = " + str(self.fc) + "Hz\n"
+            f.write(row)
+            row = "f_offset = " + str(self.offset_freq) + " Hz\n"
+            f.write(row)
+            row = "filename for this sequence begins with: " + self.fname[0:6] + ">" + self.fname[7:-4] + "_0" + self.fname[-4:] + "\n"
+            f.write(row)
+            f.write('==================================================\n')
+
+
 
 # If we run this file as the main script...
 if __name__=="__main__":
-    k = ezRxWindow('rxBins/test.bin', 6116000)
+    k = ezRxWindow()
 
 
--- rxContinuous.py
+++ rxContinuous.py
@@ -6,46 +6,61 @@ import os
 import queue
 import struct
 import argparse
+import time
 
-# Create Object
+
+# Define object
 class streamFromRadio(tk.Tk):
     # --- attributes ---
     # empty list for streamed samples
     stream_buff = []            # placeholder for our buffer of streamed samples
-    was_clicked = False          # Is set to true upon button press. Ends recording after final pkt rx.
+    was_clicked = False         # Is set to true upon button press. Ends recording after final pkt rx.
+    fileNum = 0
 
     # misc radio params
-    rx_channels = [0]
+    rx_channels = [0]           # single channel rx
 
-    def __init__(self, rx_rate, fname, fc, rx_gain, fif):
+    # flags
+    isOverLength = 0            # set when the file first exceeds the max length limit
+    fileSizeOverAck = 0         # set to prevent multiple 'file overlength' writes to the queue
+    midFileWriteFlag = 0        # set to prevent multiple file open/closes from checkQueue() function calls
+    recordTimeout = 0           # set to indicate a record timeout has occurred
+    timeOutAck = 0              # set to prevent multiple timeout messages from getting added to the queue
+
+    def __init__(self, rx_rate, fname, fc, rx_gain, fif, fileSizeLim=0, timeOutMins=0):
         # create tkinter window
         super().__init__()
-        self.geometry("200x50")
+        self.geometry("200x75")
 
-        # create label and button
+        # create labels and button
         self.lab = tk.Label(self, text="Filesize: ")
-        self.lab.pack()
+        self.lab.pack(pady=2)
 
-        self.button = tk.Button(self, text="End recording")
+        self.timeElapseLab = tk.Label(self, text="Time Elapsed: ")
+        self.timeElapseLab.pack(pady=2)
+
+        self.button = tk.Button(self, text="End recording", bg='#E55', activebackground="#E55")
         self.button['command'] = self.on_click
         self.button.pack()
 
-        # initialize radio
+        # initialize radio; copy inputs to Tk object
         self.rx_rate = rx_rate
-        self.fc = fc+fif
+        self.fif = fif
+        self.fc = fc+self.fif
         self.rx_gain = rx_gain
-        radio = self.initSdr()
+        self.fileSizeLim = fileSizeLim
+        self.radio = self.initSdr()
 
+        # index filename if a filesize limit is specified
+        self.fNameIn = fname
+        self.createFile()
 
-        # initialize file
-        self.f = open(fname, 'wb')
-
-        # write IF, fc to beginning of metadata
-        fMetadata = np.array([fc, fif, rx_rate], dtype=np.int32)
-        self.f.write(struct.pack('f'*len(fMetadata), *fMetadata))
+        # handle timings
+        self.timeOutLen = timeOutMins*60        # seconds
+        self.tStart = time.time()
 
         # begin from inside
-        self.threading(radio)  # begin recording via threaded process
+        self.threading()  # begin recording via threaded process
         self.queue = queue.Queue()              # create a queue
         self.mainloop()                         # begin tkinter mainloop
 
@@ -55,16 +70,18 @@ class streamFromRadio(tk.Tk):
         self.queue.put('terminate')
 
     # use a "main" threading function to begin RX stream
-    def threading(self, radio):
+    def threading(self):
         # Call stream function
-        t1 = Thread(target=self.streamFromRad, args=[radio])
-        t1.start()
+        self.t1 = Thread(target=self.streamFromRad, args=[self.radio])
+        self.t1.start()
 
         self.after(100, self.checkQueue)
 
     def checkQueue(self):
-        # Update tk label
+        # Update tk labels
         self.lab['text'] = "Filesize: {:.1f} MB".format(os.path.getsize(self.f.name) / 1e6)
+        self.timeElapseLab['text'] = "Time Elapsed: {:.1f} sec / {:.0f}".format(time.time()-self.tStart,
+                                                                                self.timeOutLen)
 
         """ Check if there is something in the queue. """
         try:
@@ -77,9 +94,34 @@ class streamFromRadio(tk.Tk):
             # if we do NOT get an exception (queue not empty), then check queue.
             if self.result == 'terminate':
                 self.f.close()
-                print("Acknowledging button press... file is closed.")
+                print("Acknowledging termination request...")
                 self.queue.task_done()
+                self.t1.join()
                 self.destroy()
+            elif self.result == 'fileSizeLimReached':
+                # print statement for debug
+                # print('Filesize maximum reached, closing and saving this file')
+
+                # all we do here is set the flag and jump out
+                self.isOverLength = 1
+                self.queue.task_done()
+                self.after(100, self.checkQueue)
+
+            elif self.result == 'newFileReady':
+                # reset all of our flags so we may resume streaming
+                self.isOverLength = 0
+                self.midFileWriteFlag = 0
+                self.fileSizeOverAck = 0
+                print('Writing to new file with name ' + self.f.name)
+                self.queue.task_done()
+                self.after(100, self.checkQueue)
+
+            elif self.result == 'recordTimeout':
+                # set flag and jump out
+                self.recordTimeout = 1
+                self.queue.put('terminate')
+                self.after(100, self.checkQueue)
+
             else:
                 # "catch" unknown queue elements.
                 print('unknown item in queue...')
@@ -111,45 +153,80 @@ class streamFromRadio(tk.Tk):
         nsamps = 0
 
         # begin stream from radio
-        while not self.was_clicked:
+        while not self.was_clicked and not self.recordTimeout:
         #while self.queue.empty():
-            nsamps += streamer.recv(recv_buffer, metadata)          # collect nsamps samples per loop
+            if not self.isOverLength:
+                nsamps += streamer.recv(recv_buffer, metadata)          # collect nsamps samples per loop
 
-            # interleave real and imaginary to inlv vector.
-            inlv[::2] = recv_buffer[0, :].imag
-            inlv[1::2] = recv_buffer[0, :].real
+                # interleave real and imaginary to inlv vector.
+                inlv[::2] = recv_buffer[0, :].imag
+                inlv[1::2] = recv_buffer[0, :].real
 
-            # write 32-bit floats to file; update filesize label on window.
-            self.f.write(struct.pack('f'*len(inlv), *inlv))         # 'f' datatype == float (4 bytes) for each real and imag
+                # write 32-bit floats to file; update filesize label on window.
+                self.f.write(struct.pack('f'*len(inlv), *inlv))         # 'f' datatype == float (4 bytes) for each real and imag
 
-            recv_buffer = np.zeros((1, buffer_samps), dtype=np.complex64)
+                recv_buffer = np.zeros((1, buffer_samps), dtype=np.complex64)
+                num_rx_dropped = 0
 
-            # Handle the error codes
-            if metadata.error_code == uhd.types.RXMetadataErrorCode.none:
-                # Reset the overflow flag
-                if had_an_overflow:
-                    had_an_overflow = False
-                    num_rx_dropped += (metadata.time_spec - last_overflow).to_ticks(rate)
-            elif metadata.error_code == uhd.types.RXMetadataErrorCode.overflow:
-                had_an_overflow = True
-                # Need to make sure that last_overflow is a new TimeSpec object, not
-                # a reference to metadata.time_spec, or it would not be useful
-                # further up.
-                last_overflow = uhd.types.TimeSpec(
-                    metadata.time_spec.get_full_secs(),
-                    metadata.time_spec.get_frac_secs())
-                # If we had a sequence error, record it
-                if metadata.out_of_sequence:
-                    print('sequence error')
-                # Otherwise just count the overrun
+                # Handle the error codes
+                if metadata.error_code == uhd.types.RXMetadataErrorCode.none:
+                    # Reset the overflow flag
+                    if had_an_overflow:
+                        had_an_overflow = False
+                        num_rx_dropped += (metadata.time_spec - last_overflow).to_ticks(rate)
+                elif metadata.error_code == uhd.types.RXMetadataErrorCode.overflow:
+                    had_an_overflow = True
+                    # Need to make sure that last_overflow is a new TimeSpec object, not
+                    # a reference to metadata.time_spec, or it would not be useful
+                    # further up.
+                    last_overflow = uhd.types.TimeSpec(
+                        metadata.time_spec.get_full_secs(),
+                        metadata.time_spec.get_frac_secs())
+                    # If we had a sequence error, record it
+                    if metadata.out_of_sequence:
+                        print('sequence error')
+                    # Otherwise just count the overrun
+                    else:
+                        print('overrun line 143')
+                elif metadata.error_code == uhd.types.RXMetadataErrorCode.late:
+                    print('receiver late error')
+                elif metadata.error_code == uhd.types.RXMetadataErrorCode.timeout:
+                    print('timeout')
                 else:
-                    print('overrun line 143')
-            elif metadata.error_code == uhd.types.RXMetadataErrorCode.late:
-                print('receiver late error')
-            elif metadata.error_code == uhd.types.RXMetadataErrorCode.timeout:
-                print('timeout')
+                    print("Receiver error")
+
+                # calculate filesize, check for overlength
+                if self.fileSizeLim:
+                    fSize = os.path.getsize(self.f.name) / 1e6
+                    if fSize > self.fileSizeLim and not self.fileSizeOverAck:
+                        self.fileSizeOverAck = 1
+                        self.queue.put('fileSizeLimReached')
+
+                # calculate time elapsed, check for timeout
+                if self.timeOutLen:
+                    tElapse = time.time() - self.tStart
+                    if tElapse > self.timeOutLen and not self.timeOutAck:
+                        self.timeOutAck = 1
+                        self.queue.put('recordTimeout')
+
             else:
-                print("Receiver error")
+                if not self.midFileWriteFlag:
+                    self.midFileWriteFlag = 1
+
+                    # close file
+                    self.f.close()
+
+                    # create new file with proper naming convention
+                    # fname = self.fnameTrunc + '_' + str(self.fileNum) + '.bin'
+
+                    # initialize file
+                    # self.fileNum += 1
+                    self.createFile()
+                    self.queue.put('newFileReady')
+                    self.checkQueue()
+                else:
+                    pass
+
 
     def initSdr(self):
         # TODO: throw a more intuitive error when radio is not connected
@@ -159,6 +236,21 @@ class streamFromRadio(tk.Tk):
         usrp.set_rx_gain(self.rx_gain)
         return usrp
 
+    def createFile(self):
+        if self.fileSizeLim:
+            self.fnameTrunc = self.fNameIn[:-4]
+            fname = self.fnameTrunc + '_' + str(self.fileNum) + '.bin'
+        else:
+            fname = self.fNameIn
+
+        # initialize file
+        self.f = open(fname, 'wb')
+
+        # write IF, fc to beginning of metadata
+        fMetadata = np.array([self.fc-self.fif, self.fif, self.rx_rate], dtype=np.int32)
+        self.f.write(struct.pack('f' * len(fMetadata), *fMetadata))
+        self.fileNum += 1
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", help="name of saved file", type=str, default="rx.bin")
@@ -167,6 +259,24 @@ def parse_args():
     parser.add_argument("-g", "--gain", help="set the rx gain [dB]",type=int, default=8)
     parser.add_argument("-f", "--center_freq", help="center frequency in Hz", type=float, required=True)
     return parser.parse_args()
+
+def streamRxLauncher(rx_rate, fname, fc, rx_gain, offset_freq, fsCutoff=2**9, tRecord = 10):
+    # helper function used to manage launching.
+    # default value of 2^9 = 512 MB which is the filesize cutoff we are striving for
+    tRecord = 3
+    tStart = time.time()
+    winNum = 0
+    fnameTrunc = fname[:-4]
+
+    while tStart - time.time() < tRecord*60:
+        fnameItr = fnameTrunc  + '_' + str(winNum) + '.bin'  # final 4 characters in fname are ".bin"; ignore so we can index
+        winNum += 1  # index winNum for next loop
+        k = streamFromRadio(rx_rate,
+                            fnameItr,
+                            fc,
+                            rx_gain,
+                            offset_freq,
+                            fsCutoff)
 
 if __name__=="__main__":
     # Set parameters and begin
