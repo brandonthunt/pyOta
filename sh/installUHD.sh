15670339b65a2a3536c88e7b7e524e5427c0b4f1
Cameron Sego <thomas.sego@inl.gov>
2023-01-29 16:10:42 -0700

    Add updated installUHD script

--- sh/installUHD.sh
+++ sh/installUHD.sh
@@ -3,13 +3,15 @@
 sudo apt-get install libboost-all-dev libusb-1.0-dev python-mako doxygen python-docutils cmake build-essential
 
 # clone git repo
+currDir=$(pwd)
 mkdir ~/.tempInstallDir
 cd ~/.tempInstallDir
-git clone git://github.com/EttusResearch/uhd.git
+git clone https://github.com/EttusResearch/uhd.git
 cd uhd/host
 mkdir build
 cd build
-cmake -DENABLE_TESTS=OFF -DENABLE_C_API=OFF -DENABLE_MANUAL=OFF ..
-make -j8
-sudo make install
+cmake -DENABLE_TESTS=OFF -DENABLE_C_API=OFF -DENABLE_MANUAL=OFF -B/usr/local/lib/python3/dist-packages -S/$(pwd)
+sudo make -j8
+export DESTDIR="/usr/local/lib/python3"sudo make install
 sudo ldconfig
+cd $currDir
